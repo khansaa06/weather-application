d874e9c471f22a4cddf66d8c8ce3b8a836831899
khansa Noureen <khansanoureen06@gmail.com>
2026-07-15 21:39:08 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,71 +1,35 @@
-const apiKey = "7d8688756c164d03aa463318260407";
+let display = document.getElementById("display");
 
-const searchBtn = document.getElementById("searchBtn");
-const cityInput = document.getElementById("city");
+function appendValue(value){
 
-searchBtn.addEventListener("click", getWeather);
+    display.value += value;
 
-cityInput.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-        getWeather();
+}
+
+function clearDisplay(){
+
+    display.value = "";
+
+}
+
+function deleteLast(){
+
+    display.value = display.value.slice(0,-1);
+
+}
+
+function calculate(){
+
+    try{
+
+        display.value = eval(display.value);
+
     }
-});
 
-async function getWeather() {
+    catch{
 
-    const city = cityInput.value.trim();
+        display.value = "Error";
 
-    if (city === "") {
-        alert("Please enter a city name.");
-        return;
     }
 
-    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=yes`;
-
-    try {
-
-        const response = await fetch(url);
-        const data = await response.json();
-
-        if (data.error) {
-            document.getElementById("weather").style.display = "none";
-            document.getElementById("error").innerHTML = data.error.message;
-            return;
-        }
-
-        document.getElementById("error").innerHTML = "";
-        document.getElementById("weather").style.display = "block";
-
-        document.getElementById("cityName").innerHTML =
-            `${data.location.name}, ${data.location.country}`;
-
-        document.getElementById("temperature").innerHTML =
-            `${data.current.temp_c}°C`;
-
-        document.getElementById("condition").innerHTML =
-            data.current.condition.text;
-
-        document.getElementById("icon").src =
-            "https:" + data.current.condition.icon;
-
-        document.getElementById("feelsLike").innerHTML =
-            `${data.current.feelslike_c}°C`;
-
-        document.getElementById("humidity").innerHTML =
-            `${data.current.humidity}%`;
-
-        document.getElementById("wind").innerHTML =
-            `${data.current.wind_kph} km/h`;
-
-        document.getElementById("time").innerHTML =
-            data.location.localtime;
-
-    } catch (error) {
-
-        document.getElementById("weather").style.display = "none";
-        document.getElementById("error").innerHTML =
-            "Something went wrong. Please try again.";
-
-        console.log(error);
-    }
 }
